ae05708927d99e8afbd0cee027dc60c46ac965dc
BuildTools <unconfigured@null.spigotmc.org>
2018-04-08 23:28:15 -0500

    service worker now serves style, html, and images from cache when site is offline.

--- static/sw.js
+++ static/sw.js
@@ -41,18 +41,26 @@ self.addEventListener('activate', function(event){
 
 self.addEventListener('fetch', function(event) {
 	event.respondWith(
-		fetch(event.request).then(function(response) {
-			if (response.status == 404){
-				return new Response("Whoops, not found");
-			}
-			return response;
-		}).catch(function() {
-			return new Response("Uh oh, that totally failed!");
-		}));
+		caches.match(event.request).then(function(response) {
+			if (response) return response;
+			return fetch(event.request);
+		})
+	);
+});
+
+
+		// fetch(event.request).then(function(response) {
+		// 	if (response.status == 404){
+		// 		return new Response("Whoops, not found");
+		// 	}
+		// 	return response;
+		// }).catch(function() {
+		// 	return new Response("Uh oh, that totally failed!");
+		// }));
 	// event.respondWith(
 	// 	fetch('/static/dr-evil.gif')
 	// );
-});
+// });
 
 
 
